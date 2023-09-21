8cdee2df2466ca3efee566b58ae0ad68f51e86d9
salihabrik <briksaliha@gmail.com>
2023-09-21 02:30:06 +0200

    DBStorage - Amenity

--- models/place.py
+++ models/place.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 import models
 from os import getenv
 from sqlalchemy.ext.declarative import declarative_base
@@ -23,4 +23,10 @@ class Place(BaseModel):
     longitude = Column(Float, nullable=True)
     amenity_ids = []
     
-    
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60),
+                             ForeignKey('places.id'),
+                             primary_key=True, nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'),
+                             primary_key=True, nullable=False))  
