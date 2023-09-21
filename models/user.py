bcf6735833f46a8df86ba53c361ee9b1dbbc6d9f
Halmouus <javierminimaz@gmail.com>
2023-09-21 18:10:00 +0100

    user.py - indentation

--- models/user.py
+++ models/user.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
+
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
@@ -11,7 +12,7 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-reviews = relationship('Review', cascade='all, delete, delete-orphan',
+    reviews = relationship('Review', cascade='all, delete, delete-orphan',
                            backref='user')
-places = relationship('Place', cascade='all, delete, delete-orphan',
+    places = relationship('Place', cascade='all, delete, delete-orphan',
                           backref='user')
