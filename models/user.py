03aece84d75da9b6a7e408c013ac550160e9fc2f
salihabrik <briksaliha@gmail.com>
2023-09-21 02:37:46 +0200

    DBStorage *

--- models/user.py
+++ models/user.py
@@ -11,8 +11,3 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    
-    reviews = relationship('Review', cascade='all, delete, delete-orphan',
-                           backref='user')
-    places = relationship('Place', cascade='all, delete, delete-orphan',
-                          backref='user')
