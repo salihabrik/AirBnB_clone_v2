0c5a760578916937ad75995f70fa677abf12e2d8
Halmouus <javierminimaz@gmail.com>
2023-09-23 13:00:57 +0100

    revert back to previous file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,14 +16,10 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of instances of a class currently in storage"""
-        if cls:
-            dictionary = {}
-            for k, v in self.__objects.items():
-                if cls == v.__class__ or cls == v.__class__.__name__:
-                    dictionary[k] = v
-            return dictionary
+        if (cls):
+            return {k: v for k,v in self.__objects.items() 
+                    if type(v) == cls}
         return self.__objects
-
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
